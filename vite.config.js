6d05ad9590c898a202e121d6e96caaddfef140e8
Khairya Zedan <khairya.abdelraheem@gmail.com>
2026-04-04 06:56:06 +0200

    Remove module alias configuration from Vite config
    Removed alias configuration for module resolution.

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  resolve:{
-    alias:{
-      "@": 'src',
-      "@components/*": ["src/components/*"],
-    },
+  
 
   },
 })
